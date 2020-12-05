4afb4f49a8aed6998369488ee03270ff8c5f7c36
Genta Kamitani <oftn.mofumofu@gmail.com>
2020-12-05 22:37:25 +0900

    :recycle: refactor: move test for DeepCopy to vm_test.go

--- pkg/vm/execution_test.go
+++ pkg/vm/execution_test.go
@@ -1,7 +1,6 @@
 package vm
 
 import (
-	"bytes"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -591,87 +590,5 @@ func TestFeedMultiExecutesOpsSequentially(t *testing.T) {
 
 	if diff := cmp.Diff(want, got); diff != "" {
 		t.Errorf("mismatch (-want +got):\n%s", diff)
-	}
-}
-
-func TestDeepCopyWithInt(t *testing.T) {
-	orig := NewIntValue(123)
-	clone := orig.DeepCopy()
-	if diff := cmp.Diff(orig, clone); diff != "" {
-		t.Errorf("mismatch (-want +got):\n%s", diff)
-	}
-
-	clone.Int = 456
-	if orig.Int == clone.Int {
-		t.Errorf("DeepCopy returned receiver itself")
-	}
-}
-
-func TestDeepCopyString(t *testing.T) {
-	orig := NewStringValue([]byte("hello"))
-	clone := orig.DeepCopy()
-	if diff := cmp.Diff(orig, clone); diff != "" {
-		t.Errorf("mismatch (-want +got):\n%s", diff)
-	}
-
-	clone.String[0] = 0x61 // 'a'
-	if bytes.Equal(orig.String, clone.String) {
-		t.Errorf("clone shares the same reference with its origin")
-	}
-
-	clone.String = []byte("world")
-	if bytes.Equal(orig.String, clone.String) {
-		t.Errorf("DeepCopy returned receiver itself")
-	}
-}
-
-func TestDeepCopyWithObject(t *testing.T) {
-	orig := NewObjectValue(map[string]*Value{
-		"hello": NewStringValue([]byte("world")),
-	})
-	clone := orig.DeepCopy()
-	if diff := cmp.Diff(orig, clone); diff != "" {
-		t.Errorf("mismatch (-want +got):\n%s", diff)
-	}
-
-	clone.Object["hello"].String[0] = 0x61 // 'a'
-	if diff := cmp.Diff(orig, clone); diff == "" {
-		t.Errorf("clone shares the same reference with its origin")
-	}
-
-	clone.Object["hoge"] = NewStringValue([]byte("fuga"))
-	if diff := cmp.Diff(orig, clone); diff == "" {
-		t.Errorf("clone shares the same reference with its origin")
-	}
-
-	clone.Object = map[string]*Value{}
-	if diff := cmp.Diff(orig, clone); diff == "" {
-		t.Errorf("DeepCopy returned receiver itself")
-	}
-}
-
-func TestDeepCopyWithBool(t *testing.T) {
-	orig := NewBoolValue(true)
-	clone := orig.DeepCopy()
-	if diff := cmp.Diff(orig, clone); diff != "" {
-		t.Errorf("mismatch (-want +got):\n%s", diff)
-	}
-
-	clone.Bool = false
-	if orig.Bool == clone.Bool {
-		t.Errorf("DeepCopy returned receiver itself")
-	}
-}
-
-func TestDeepCopyWithNil(t *testing.T) {
-	orig := NewNilValue()
-	clone := orig.DeepCopy()
-	if diff := cmp.Diff(orig, clone); diff != "" {
-		t.Errorf("mismatch (-want +got):\n%s", diff)
-	}
-
-	clone.Kind = KInt
-	if orig.Kind == clone.Kind {
-		t.Errorf("DeepCopy returned receiver itself")
 	}
 }
