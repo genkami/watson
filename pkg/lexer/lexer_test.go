0dbe251a342d96168f5a9f6031b1d2cd4859467c
Genta Kamitani <oftn.mofumofu@gmail.com>
2020-12-06 00:27:22 +0900

    :100: test: move test for opTable to repr_test.go

--- pkg/lexer/lexer_test.go
+++ pkg/lexer/lexer_test.go
@@ -1,39 +1,9 @@
 package lexer
 
 import (
-	"bytes"
 	"testing"
-
-	"github.com/genkami/watson/pkg/vm"
 )
 
 func TestOpTable(t *testing.T) {
-	table := map[string]vm.Op{
-		"Y": vm.Inew,
-		"u": vm.Iinc,
-		"m": vm.Ishl,
-		"y": vm.Iadd,
-		"?": vm.Snew,
-		"!": vm.Sadd,
-		"~": vm.Onew,
-		"M": vm.Oadd,
-		"z": vm.Bnew,
-		"o": vm.Bneg,
-		".": vm.Nnew,
-	}
-	for s, expected := range table {
-		actual, err := readOne(s)
-		if err != nil {
-			t.Fatalf("error on testing %s (%#v): %#v", s, expected, err)
-		}
-		if expected != actual {
-			t.Errorf("expected %#v but got %#v", expected, actual)
-		}
-	}
-}
-
-func readOne(s string) (vm.Op, error) {
-	buf := bytes.NewReader([]byte(s))
-	l := NewLexer(buf)
-	return l.Next()
+	// nop for now
 }
---
+++ pkg/repr/repr_test.go
@@ -0,0 +1,43 @@
+package repr
+
+import (
+	"testing"
+
+	"github.com/genkami/watson/pkg/vm"
+)
+
+var table = map[string]vm.Op{
+	"Y": vm.Inew,
+	"u": vm.Iinc,
+	"m": vm.Ishl,
+	"y": vm.Iadd,
+	"?": vm.Snew,
+	"!": vm.Sadd,
+	"~": vm.Onew,
+	"M": vm.Oadd,
+	"z": vm.Bnew,
+	"o": vm.Bneg,
+	".": vm.Nnew,
+}
+
+func TestReadOp(t *testing.T) {
+	for s, expected := range table {
+		actual, ok := ReadOp([]byte(s)[0])
+		if !ok {
+			t.Fatalf("op not found for %s", s)
+		}
+		if expected != actual {
+			t.Errorf("expected %#v but got %#v", expected, actual)
+		}
+	}
+}
+
+func TestShowOp(t *testing.T) {
+	for s, op := range table {
+		expected := []byte(s)[0]
+		actual := ShowOp(op)
+		if expected != actual {
+			t.Errorf("expected %#v but got %#v", expected, actual)
+		}
+	}
+}
