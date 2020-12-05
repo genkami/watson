2a2420463f500f0aae41a23fdae2427dd33137e1
Genta Kamitani <oftn.mofumofu@gmail.com>
2020-12-06 00:21:49 +0900

    :+1: feat: add pkg/repr

--- pkg/lexer/lexer.go
+++ pkg/lexer/lexer.go
@@ -3,6 +3,7 @@ package lexer
 import (
 	"io"
 
+	"github.com/genkami/watson/pkg/repr"
 	"github.com/genkami/watson/pkg/vm"
 )
 
@@ -17,20 +18,6 @@ func NewLexer(r io.Reader) *Lexer {
 	return &Lexer{r: r}
 }
 
-var opTable = map[byte]vm.Op{
-	0x59: vm.Inew, // 'Y'
-	0x75: vm.Iinc, // 'u'
-	0x6d: vm.Ishl, // 'm'
-	0x79: vm.Iadd, // 'y'
-	0x3f: vm.Snew, // '?'
-	0x21: vm.Sadd, // '!'
-	0x7e: vm.Onew, // '~'
-	0x4d: vm.Oadd, // 'M'
-	0x7a: vm.Bnew, // 'z'
-	0x6f: vm.Bneg, // 'o'
-	0x2e: vm.Nnew, // '.'
-}
-
 // Returns the next Op.
 // This returns io.EOF if it hits on the end of the input.
 func (l *Lexer) Next() (vm.Op, error) {
@@ -40,7 +27,7 @@ func (l *Lexer) Next() (vm.Op, error) {
 			// Note that it returns io.EOF if the underlying Reader returns io.EOF.
 			return 0, err
 		}
-		if op, ok := opTable[l.buf[0]]; ok {
+		if op, ok := repr.ReadOp(l.buf[0]); ok {
 			return op, nil
 		}
 	}
---
+++ pkg/repr/repr.go
@@ -0,0 +1,46 @@
+// Package repr is responsible for Watson Representation or an ASCII representation of Watson's opcodes.
+package repr
+
+import (
+	"fmt"
+
+	"github.com/genkami/watson/pkg/vm"
+)
+
+var opTable = map[byte]vm.Op{
+	0x59: vm.Inew, // 'Y'
+	0x75: vm.Iinc, // 'u'
+	0x6d: vm.Ishl, // 'm'
+	0x79: vm.Iadd, // 'y'
+	0x3f: vm.Snew, // '?'
+	0x21: vm.Sadd, // '!'
+	0x7e: vm.Onew, // '~'
+	0x4d: vm.Oadd, // 'M'
+	0x7a: vm.Bnew, // 'z'
+	0x6f: vm.Bneg, // 'o'
+	0x2e: vm.Nnew, // '.'
+}
+
+var reversedTable map[vm.Op]byte
+
+func init() {
+	reversedTable = make(map[vm.Op]byte)
+	for k, v := range opTable {
+		reversedTable[v] = k
+	}
+}
+
+// Returns a Op that corresponds to the given byte.
+// This returns false if and only if b is not in the byte-to-op map.
+func ReadOp(b byte) (op vm.Op, ok bool) {
+	op, ok = opTable[b]
+	return
+}
+
+// Returns a ascii representation of the given Op.
+func ShowOp(op vm.Op) byte {
+	if b, ok := reversedTable[op]; ok {
+		return b
+	}
+	panic(fmt.Errorf("unknown Op: %#v\n", op))
+}
