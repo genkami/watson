01cc2a2f24f53909ef47ca6be09a153fb1c5009e
Genta Kamitani <oftn.mofumofu@gmail.com>
2020-12-13 15:38:54 +0900

    :recycle: refactor: move repr to lexer

--- pkg/lexer/lexer.go
+++ pkg/lexer/lexer.go
@@ -1,11 +1,73 @@
+// Package lexer provides a way to convert a byte sequence into a sequence of Watson's instructions and vice versa.
 package lexer
 
 import (
+	"fmt"
 	"io"
 
-	"github.com/genkami/watson/pkg/repr"
 	"github.com/genkami/watson/pkg/vm"
 )
+
+// Mode is an important concept that is unique to Watson.
+// It determines the correspondence between Vm's instructions and their representation.
+type Mode int
+
+const (
+	A Mode = iota // A is the initial mode of the lexer. See the specification for more details.
+)
+
+var opTableA = map[byte]vm.Op{
+	char("B"): vm.Inew,
+	char("u"): vm.Iinc,
+	char("b"): vm.Ishl,
+	char("a"): vm.Iadd,
+	char("A"): vm.Ineg,
+	char("e"): vm.Isht,
+	char("i"): vm.Itof,
+	char("q"): vm.Finf,
+	char("t"): vm.Fnan,
+	char("p"): vm.Fneg,
+	char("?"): vm.Snew,
+	char("!"): vm.Sadd,
+	char("~"): vm.Onew,
+	char("M"): vm.Oadd,
+	char("@"): vm.Anew,
+	char("s"): vm.Aadd,
+	char("z"): vm.Bnew,
+	char("o"): vm.Bneg,
+	char("."): vm.Nnew,
+	char("*"): vm.Gdup,
+	char("#"): vm.Gpop,
+	char("%"): vm.Gswp,
+}
+
+var reversedTableA map[vm.Op]byte
+
+func init() {
+	reversedTableA = make(map[vm.Op]byte)
+	for k, v := range opTableA {
+		reversedTableA[v] = k
+	}
+}
+
+// Returns a Op that corresponds to the given byte.
+// This returns false if and only if b is not in the byte-to-op map.
+func ReadOp(m Mode, b byte) (op vm.Op, ok bool) {
+	op, ok = opTableA[b]
+	return
+}
+
+// Returns a ascii representation of the given Op.
+func ShowOp(m Mode, op vm.Op) byte {
+	if b, ok := reversedTableA[op]; ok {
+		return b
+	}
+	panic(fmt.Errorf("unknown Op: %#v\n", op))
+}
+
+func char(s string) byte {
+	return []byte(s)[0]
+}
 
 // Lexer is responsible for converting a Watson Representation into a sequence of vm.Ops.
 type Lexer struct {
@@ -27,7 +89,7 @@ func (l *Lexer) Next() (vm.Op, error) {
 			// Note that it returns io.EOF if the underlying Reader returns io.EOF.
 			return 0, err
 		}
-		if op, ok := repr.ReadOp(repr.A, l.buf[0]); ok {
+		if op, ok := ReadOp(A, l.buf[0]); ok {
 			return op, nil
 		}
 	}
--- pkg/lexer/lexer_test.go
+++ pkg/lexer/lexer_test.go
@@ -8,6 +8,25 @@ import (
 	"github.com/genkami/watson/pkg/vm"
 )
 
+func TestOpTableIsSurjectiveWhenModeIsA(t *testing.T) {
+	ops := map[vm.Op]bool{}
+	for _, op := range vm.AllOps() {
+		ops[op] = true
+	}
+	for _, op := range opTableA {
+		delete(ops, op)
+	}
+	for op := range ops {
+		t.Errorf("%#v is not in opTable", op)
+	}
+}
+
+func TestShowOpIsDefinedForAllOpsWhenModeIsA(t *testing.T) {
+	for _, op := range vm.AllOps() {
+		ShowOp(A, op)
+	}
+}
+
 func TestNextReturnsTheFirstOp(t *testing.T) {
 	op, err := readOne("B")
 	if err != nil {
